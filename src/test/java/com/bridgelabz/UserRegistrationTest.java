24dc9e3764c9223296e385db942e5fd0b9923c06
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-10 07:53:15 +0530

    Welcome

---
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    @Test
+    void given_first_name_should_returnTrue() {
+        UserValidation validation = new UserValidation();
+    }
+}
+
+
