bf32e997cf1b722a6cb6079fe60abd524a55cc8c
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-11-18 08:43:11 +0530

    Added:UC9_Validate_Email_all_samples

--- src/main/java/com/bridgelabz/UserValidation.java
+++ src/main/java/com/bridgelabz/UserValidation.java
@@ -3,47 +3,48 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-    public static final String VALIDATE_NAME = "^[A-z]+[a-z]{2,}$";
-    public static final String EMAIL_ID = "^[A-Za-z]{3,}[.]?[A-Za-z]{2,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
-    public static final String PHONE_NUMBER = "^[0-9]+\s[0-9]{10}$";
-    public static final String PASSWORD_RULE1 = "^[a-z]{8,}$";
+    public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
+    public static final String EMAIL_ID="^[A-Za-z]{3,}[.-]?[A-Za-z]{2,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
+    public static final String PHONE_NUMBER="^[0-9]+\s[0-9]{10}$";
+    public static final String PASSWORD_RULE1="^[a-z]{8,}$";
     public static final String PASSWORD_RULE2="^[A-Z]+[a-z]{7,}$";
     public static final String PASSWORD_RULE3="^[A-Z]+[a-z0-9]{7,}$";
     public static final String PASSWORD_RULE4="^[A-Z]+[a-z0-9]{7,}[!@#$%&*]+$";
     public static final String SEPERATE_EMAIL_ID="^[A-Za-z]{3,}[.+-]?[A-Za-z0-9]*[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,}[.]?[a-z]*$";
-    public boolean checkFirstName(String firstName) {
+
+    public Boolean checkFirstName(String firstName) {
         return (Pattern.matches(VALIDATE_NAME, firstName));
     }
 
     public Boolean checkLastName(String lastName) {
-        return (Pattern.matches(VALIDATE_NAME, lastName));
+        return (Pattern.matches(VALIDATE_NAME,lastName));
     }
 
     public Boolean checkemailId(String emailId) {
-        return (Pattern.matches(EMAIL_ID, emailId));
+        return (Pattern.matches(EMAIL_ID,emailId));
     }
 
     public Boolean phoneNumber(String phoneNumber) {
-        return (Pattern.matches(PHONE_NUMBER, phoneNumber));
-
+        return (Pattern.matches(PHONE_NUMBER,phoneNumber));
     }
 
     public Boolean password(String password) {
-        return (Pattern.matches(PASSWORD_RULE1, password));
+        return (Pattern.matches(PASSWORD_RULE1,password));
     }
 
     public Boolean password2(String password1) {
-        return (Pattern.matches(PASSWORD_RULE2, password1));
+        return (Pattern.matches(PASSWORD_RULE2,password1));
     }
+
     public Boolean password3(String password2) {
         return (Pattern.matches(PASSWORD_RULE3,password2));
     }
+
     public Boolean password4(String password3) {
         return (Pattern.matches(PASSWORD_RULE4,password3));
     }
+
     public Boolean checkEmailSeperate(String checkEmailSeperate) {
         return (Pattern.matches(SEPERATE_EMAIL_ID,checkEmailSeperate));
     }
-
 }
-
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -4,16 +4,18 @@ package com.bridgelabz;
 import org.junit.jupiter.api.*;
 
 public class UserRegistrationTest {
-    @BeforeEach
-    void intiliztion() {
-        System.out.println("Before Each");
-
+    @BeforeAll
+    static void beforeAllTestMsg() {
+        System.out.println("Welcome to User Registration Problem");
+    }
+    @AfterAll
+    static void afterAllTestPassedMsg(){
+        System.out.println("All Test Case Passed");
     }
 
     @AfterEach
-    void inilization() {
+    public void afterEachTestPassMsg() {
         System.out.println("Passed");
-
     }
 
     @Test
@@ -21,7 +23,6 @@ public class UserRegistrationTest {
         UserValidation validation = new UserValidation();
         Boolean result = validation.checkFirstName("Namdev");
         Assertions.assertEquals(result, true);
-
     }
 
     @Test
@@ -29,7 +30,6 @@ public class UserRegistrationTest {
         UserValidation validation = new UserValidation();
         Boolean result1 = validation.checkLastName("Chavan");
         Assertions.assertEquals(result1, true);
-
     }
 
     @Test
@@ -45,44 +45,33 @@ public class UserRegistrationTest {
         Boolean result3 = validation.phoneNumber("91 9604411890");
         Assertions.assertEquals(result3, true);
     }
-
     @Test
     void given_password_should_returnTrue() {
         UserValidation validation = new UserValidation();
         Boolean result4 = validation.password("ksndfkdn");
         Assertions.assertEquals(result4, true);
     }
-
     @Test
     void given_password1_should_returnTrue() {
         UserValidation validation = new UserValidation();
         Boolean result5 = validation.password2("Ksndfkdn");
         Assertions.assertEquals(result5, true);
     }
-
     @Test
     void given_password2_should_returnTrue() {
         UserValidation validation = new UserValidation();
-        Boolean result6 = validation.password3("Ksndkdv67");
+        Boolean result6 = validation.password3("Ksndkdv98");
         Assertions.assertEquals(result6, true);
-    }
-
-    @Test
+    }  @Test
     void given_password3_should_returnTrue() {
         UserValidation validation = new UserValidation();
-        Boolean result7 = validation.password4("Ksndkdv67@");
+        Boolean result7 = validation.password4("Ksndkdv98@");
         Assertions.assertEquals(result7, true);
     }
-
     @Test
-    void given_emailId_should_return_True() {
+    void given_emailId_should_return_True(){
         UserValidation validation = new UserValidation();
         Boolean result8 = validation.checkEmailSeperate("abc@yahoo.com");
         Assertions.assertEquals(result8, true);
     }
 }
-
-
-
-
-
